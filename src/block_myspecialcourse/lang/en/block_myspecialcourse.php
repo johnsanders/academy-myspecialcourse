7c0962ee2c87daf7ffc0e98012f06e59b1155764
John Sanders <john.sanders@warnermedia.com>
2022-09-18 13:51:16 +0100

    Clean up

--- src/block_myspecialcourse/lang/en/block_myspecialcourse.php
+++ src/block_myspecialcourse/lang/en/block_myspecialcourse.php
@@ -3,7 +3,7 @@
 $string['myspecialcourse:addinstance'] = 'Add a new My Special Course block';
 $string['myspecialcourse:myaddinstance'] = 'Add a new My Special Course block to Dashboard';
 $string['myspecialcourse'] = 'CNN Academy: My Special Course';
-$string['pluginname'] = 'CNN Academy: My Special Courses';
+$string['pluginname'] = 'CNN Academy: My Special Course';
 $string['courseid'] = 'Special Course ID';
 $string['title'] = 'Title';
 $string['myresources'] = 'My Resources';
