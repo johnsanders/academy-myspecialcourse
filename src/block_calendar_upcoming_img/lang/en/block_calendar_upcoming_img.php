61713b38bc7292c15306342214dbf50484f7854c
John Sanders <john.sanders@warnermedia.com>
2022-09-18 17:30:24 +0100

    Fix localization

--- src/block_calendar_upcoming_img/lang/en/block_calendar_upcoming_img.php
+++ src/block_calendar_upcoming_img/lang/en/block_calendar_upcoming_img.php
@@ -1,6 +1,5 @@
 <?php
 
-$string['myspecialcourse:addinstance'] = 'Add a new Upcoming Events with Images block';
-$string['myspecialcourse:myaddinstance'] = 'Add a new Upcoming Events with Images block to Dashboard';
-$string['myspecialcourse'] = 'CNN Academy: My Special Course';
+$string['calendar_upcoming_img:addinstance'] = 'Add a new Upcoming Events with Images block';
+$string['calendar_upcoming_img:myaddinstance'] = 'Add a new Upcoming Events with Images block to Dashboard';
 $string['pluginname'] = 'CNN Academy: Upcoming Events with Images';
